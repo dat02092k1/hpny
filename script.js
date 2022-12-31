d098256b263314ba41de37acbf3e5c6d49ac03bb
Dat Ngo <ngodat02092001@gmail.com>
2022-12-31 23:57:49 +0700

    up

---
+++ script.js
@@ -0,0 +1,35 @@
+window.alert('Hehh, chúc mừng năm mới 2023! Nhận được tin nhắn này tức là bạn là một người đặc biệt với tôi, năm mới đã đến rồi, tôi biết chúng ta xảy ra nhiều bất đồng và ý kiến trong năm vừa qua, nhưng hy vọng năm mới, chúng ta sẽ nhìn nhau bằng con người mới, ánh mắt mới, bỏ qua mọi hiểu lầm và chuyện cũ, tha thứ cho nhau. Anyways, chúc năm mới vui vẻ, bạn của tôi !!!');
+
+var design = anime({
+    targets: '#newyear2020 #happy',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2000,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+  
+  var design = anime({
+    targets: '#newyear2020 #NEWYEAR',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+  
+  
+  
+  var design = anime({
+    targets: '#newyear2020 #Vector_43,#Vector_210,#Vector_207,#Vector_42,#Vector_45',
+    translateY: -10,
+    easing: 'easeInOutSine',
+    duration: 2500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+  
+  
